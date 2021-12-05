02b9f7ffdaa3359873ddf504474ff5219ea9ee95
Matthieu Mourisson <mareek@gmail.com>
2021-12-05 18:51:27 +0100

    Add more image to encode/decode tests

--- QOI.NET/QoiEncoder.cs
+++ QOI.NET/QoiEncoder.cs
@@ -13,33 +13,40 @@ namespace QOI.NET
         public byte[] Write(Bitmap image)
         {
             using var stream = new MemoryStream();
-            stream.Write(FromatConstants.MagicBytes);
 
-            Span<byte> uintBuffer = stackalloc byte[4];
-            BinaryPrimitives.WriteUInt32BigEndian(uintBuffer, (uint)image.Width);
-            stream.Write(uintBuffer);
-            BinaryPrimitives.WriteUInt32BigEndian(uintBuffer, (uint)image.Height);
-            stream.Write(uintBuffer);
-
-            /*
- struct qoi_header_t {
-    char [4];       // magic bytes "qoif"
-    u32 width;      // image width in pixels (BE)
-    u32 height;     // image height in pixels (BE)
-     u8 channels;   // must be 3 (RGB) or 4 (RGBA)
-     u8 colorspace; // a bitmap 0000rgba where 
-                    //   - a zero bit indicates sRGBA, 
-                    //   - a one bit indicates linear (user interpreted)
-                    //   colorspace for each channel
-};
- */
-
-            stream.WriteByte(4); //Channels
-            stream.WriteByte(0b0000_1111); //color space
-
+            WriteHeader(image, stream);
             EncodePixels(image.GetPixels(), stream);
 
             return stream.ToArray();
+        }
+
+        private static void WriteHeader(Bitmap image, MemoryStream stream)
+        {
+            /*
+             struct qoi_header_t {
+                char [4];       // magic bytes "qoif"
+                u32 width;      // image width in pixels (BE)
+                u32 height;     // image height in pixels (BE)
+                 u8 channels;   // must be 3 (RGB) or 4 (RGBA)
+                 u8 colorspace; // a bitmap 0000rgba where 
+                                //   - a zero bit indicates sRGBA, 
+                                //   - a one bit indicates linear (user interpreted)
+                                //   colorspace for each channel
+            };
+             */
+
+            stream.Write(FromatConstants.MagicBytes);
+            
+            Span<byte> widthBuffer = stackalloc byte[4];
+            BinaryPrimitives.WriteUInt32BigEndian(widthBuffer, (uint)image.Width);
+            stream.Write(widthBuffer);
+
+            Span<byte> heightBuffer = stackalloc byte[4];
+            BinaryPrimitives.WriteUInt32BigEndian(heightBuffer, (uint)image.Height);
+            stream.Write(heightBuffer);
+
+            stream.WriteByte(4); //Channels
+            stream.WriteByte(0b0000_0000); //color space
         }
 
         private void EncodePixels(IEnumerable<Color> pixels, Stream stream)
--- QOI.Test/IntegrationTest.cs
+++ QOI.Test/IntegrationTest.cs
@@ -7,16 +7,33 @@ namespace QOI.Test;
 
 public class IntegrationTest
 {
+    private static readonly Color[] SimpleColors =
+    {
+        Color.Chartreuse,
+        Color.Bisque,
+        Color.Aquamarine,
+        Color.OrangeRed,
+        Color.Crimson,
+        Color.LemonChiffon,
+        Color.PeachPuff,
+        Color.AntiqueWhite,
+        Color.BlanchedAlmond,
+        Color.CornflowerBlue,
+        Color.BurlyWood,
+        Color.Firebrick,
+        Color.RosyBrown,
+        Color.DeepSkyBlue,
+    };
+
     [Theory]
     [MemberData(nameof(GetImgTest))]
-    public void DoMyTest(Bitmap imgTest)
+    public void FullCircleProcessTest(Bitmap imgTest)
     {
-
-        var encoder = new QoiEncoder();
+        QoiEncoder encoder = new();
         var imgBytes = encoder.Write(imgTest);
-        
-        var decodeStream = new MemoryStream(imgBytes);
-        var decoder = new QoiDecoder();
+
+        using MemoryStream decodeStream = new(imgBytes);
+        QoiDecoder decoder = new();
         var decodedImage = decoder.Read(decodeStream);
 
         for (int y = 0; y < imgTest.Height; y++)
@@ -32,14 +49,55 @@ public class IntegrationTest
 
     public static IEnumerable<object[]> GetImgTest()
     {
-        const int width = 8;
-        const int height = 6;
-
-        yield return new object[] { new Bitmap(width, height) };
+        Bitmap blankImage = new(8, 6);
+        yield return new object[] { blankImage };
+        yield return new object[] { GetRandomStripedImage() };
+        yield return new object[] { GetRandomSimpleImage() };
+        yield return new object[] { GetRandomArgbImage() };
     }
 
-    private void SetRandomPixels(Bitmap image)
+    private static Bitmap GetRandomStripedImage()
     {
-        
+        Bitmap stripedImage = new(8, 6);
+        for (int y = 0; y < stripedImage.Height; y++)
+        {
+            var color = SimpleColors[Random.Shared.Next(0, SimpleColors.Length)];
+            for (int x = 0; x < stripedImage.Width; x++)
+            {
+                stripedImage.SetPixel(x, y, color);
+            }
+        }
+        return stripedImage;
+    }
+
+    private static Bitmap GetRandomSimpleImage()
+    {
+        Bitmap simpleImage = new(8, 6);
+        for (int y = 0; y < simpleImage.Height; y++)
+        {
+            for (int x = 0; x < simpleImage.Width; x++)
+            {
+                var color = SimpleColors[Random.Shared.Next(0, SimpleColors.Length)];
+                simpleImage.SetPixel(x, y, color);
+            }
+        }
+        return simpleImage;
+    }
+
+    private static Bitmap GetRandomArgbImage()
+    {
+        Bitmap simpleImage = new(8, 6);
+        for (int y = 0; y < simpleImage.Height; y++)
+        {
+            for (int x = 0; x < simpleImage.Width; x++)
+            {
+                var color = Color.FromArgb(Random.Shared.Next(0, 256),
+                                           Random.Shared.Next(0, 256),
+                                           Random.Shared.Next(0, 256),
+                                           Random.Shared.Next(0, 256));
+                simpleImage.SetPixel(x, y, color);
+            }
+        }
+        return simpleImage;
     }
 }
