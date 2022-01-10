b5809ca0e4240e4634298ae79141c281c403857b
Matthieu Mourisson <mareek@gmail.com>
2022-01-10 01:53:14 +0100

    Refactoring to ease future implementation of imageriter

--- QOI.Core/Interface/IImageWriter.cs
+++ QOI.Core/Interface/IImageWriter.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace QOI.Core.Interface;
 
-namespace QOI.Core.Interface
+public interface IImageWriter
 {
-    public interface IImageWriter
-    {
-        void Init(uint width, uint height, bool hasAlpha, bool isSrgb);
-        void WritePixel(byte r, byte g, byte b, byte a);
+    bool IsComplete { get; }
 
-        bool IsComplete { get; }
-    }
+    void Init(uint width, uint height, bool hasAlpha, bool isSrgb);
+    void WritePixel(byte r, byte g, byte b, byte a);
 }
--- QOI.Viewer/BitmapSourceImageWriter.cs
+++ QOI.Viewer/BitmapSourceImageWriter.cs
@@ -4,32 +4,16 @@ using QOI.Core.Interface;
 
 namespace QOI.Viewer;
 
-internal class BitmapSourceImageWriter : IImageWriter
+internal class BitmapSourceImageWriter : RawPixelsImageWriter
 {
-    private int _width;
-    private int _height;
-    private int _pixelSize = 4;
-    private byte[]? _rawPixels;
-    private int _currentIndex;
-    public bool IsComplete => _rawPixels != null && _rawPixels.Length <= _currentIndex;
+    protected override int PixelSize => 4;
 
-    public void Init(uint width, uint height, bool hasAlpha, bool isSrgb)
+    protected override void WritePixelBytes(byte r, byte g, byte b, byte a, Span<byte> buffer)
     {
-        _width = (int)width;
-        _height = (int)height;
-        _rawPixels = new byte[_width * _height * _pixelSize];
-    }
-
-    public void WritePixel(byte r, byte g, byte b, byte a)
-    {
-        if (_rawPixels == null) throw new ArgumentNullException("Image has not been initialized");
-
-        _rawPixels[_currentIndex] = b;
-        _rawPixels[_currentIndex + 1] = g;
-        _rawPixels[_currentIndex + 2] = r;
-        _rawPixels[_currentIndex + 3] = a;
-
-        _currentIndex += _pixelSize;
+        buffer[0] = b;
+        buffer[1] = g;
+        buffer[2] = r;
+        buffer[3] = a;
     }
 
     public BitmapSource GetImage()
@@ -43,6 +27,6 @@ internal class BitmapSourceImageWriter : IImageWriter
                                    System.Windows.Media.PixelFormats.Bgra32,
                                    null,
                                    _rawPixels,
-                                   _width * _pixelSize);
+                                   _width * PixelSize);
     }
 }
